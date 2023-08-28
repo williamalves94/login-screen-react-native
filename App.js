34192b04a359522b2c742c5d251b59d5689fc52b
William Alves <williamcostacardoso94@gmail.com>
2023-08-27 21:55:49 -0300

    feat: teste

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Funcionando</Text>
+      <Text>Funcionando!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
